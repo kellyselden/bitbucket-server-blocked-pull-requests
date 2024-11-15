5950a3b798d6840dc09eb6829df3e4609e63895a
Kelly Selden <kellyselden@gmail.com>
2024-11-15 14:04:46 -0800

    url

--- user.js
+++ user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Bitbucket Server Blocked Pull Requests
 // @namespace    https://github.com/kellyselden
-// @version      4
+// @version      5
 // @description  Show which pull requests are blocked without opening them
 // @updateURL    https://raw.githubusercontent.com/kellyselden/bitbucket-server-blocked-pull-requests/main/meta.js
 // @downloadURL  https://raw.githubusercontent.com/kellyselden/bitbucket-server-blocked-pull-requests/main/user.js
 // @author       Kelly Selden
 // @license      MIT
-// @supportURL   https://github.com/kellyselden/bitbucket-server-blocked-pull-requests
+// @source       https://github.com/kellyselden/bitbucket-server-blocked-pull-requests
+// @supportURL   https://github.com/kellyselden/bitbucket-server-blocked-pull-requests/issues/new
 // @match        http*://*bitbucket*/dashboard
 // @match        http*://*bitbucket*/projects/*/repos/*/pull-requests*
 // ==/UserScript==
